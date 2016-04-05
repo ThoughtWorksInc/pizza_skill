48244401948d088a3928db6ad655d6392cc4d648
Colin Harris <col.w.harris@gmail.com>
2016-04-05 19:17:01 +1000

    Add the request as a param to all the EventHandler functions.
    This is required to allow access to the user_id. We could just expose the user_id but I thought the request would be more generic.

--- lib/pizza_skill/event_handler_impl.ex
+++ lib/pizza_skill/event_handler_impl.ex
@@ -1,6 +1,6 @@
 defimpl PizzaSkill.EventHandler, for: Any do
-  def start_order(order), do: order
-  def add_item(order), do: order
-  def remove_item(order), do: order
-  def confirm_order(order), do: order
+  def start_order(order, request), do: order
+  def add_item(order, request), do: order
+  def remove_item(order, request), do: order
+  def confirm_order(order, request), do: order
 end
